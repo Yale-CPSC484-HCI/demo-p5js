2e6d060deaa8f40d01c8de9d4cf45c9f82aace22
ann <71188235+annleez@users.noreply.github.com>
2023-04-06 14:24:59 -0400

    style changes to accommodate colorblindness. colorblind-friendly palettes generated using colorgorical.

--- sketch.js
+++ sketch.js
@@ -116,10 +116,10 @@ function setup() {
 }
 
 function draw() {
-  background('#b1d1fc');
+  background('rgb(150,204,254)');
   for (let i = 0; i < numSegments - 1; i++) {
     line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
-    stroke('#c1f80a');
+    stroke('rgb(141,25,147)');
   }
   updateSnakeCoordinates();
   updateHandContainer();
