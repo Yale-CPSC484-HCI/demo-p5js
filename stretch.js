dc49bf4203c049bf9fdc79a06b143e197d0601c3
Jason Yao <yifeiy6@illinois.edu>
2023-04-23 18:57:56 -0400

    update 2

--- stretch.js
+++ stretch.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 	frames.start();
 	twod.start();
 });
+var positions = [];
 
 var frames = {
 	socket: null,
@@ -11,11 +12,12 @@ var frames = {
 		var url = 'ws://' + host + '/frames';
 		frames.socket = new WebSocket(url);
 		frames.socket.onmessage = function (event) {
-			var command = frames.timer_function(JSON.parse(event.data));
+			var command = frames.get_left_wrist_command(JSON.parse(event.data));
 			// if (command !== null) {
 			// 	sendWristCommand(command);
 			// }
 		};
+        this.timer_function();
 	},
 
 	timer_function: function (frame) {
@@ -44,37 +46,37 @@ var frames = {
                 command = 0;
 
                 // Normalize by subtracting the root (pelvis) joint coordinates
-                var chest_x = frame.people[0].joints[2].position.x;
-                var chest_y = frame.people[0].joints[2].position.y;
-                var chest_z = frame.people[0].joints[2].position.z;
-                var nose_x = frame.people[0].joints[27].position.x;
-                var nose_y = frame.people[0].joints[27].position.y;
-                var nose_z = frame.people[0].joints[27].position.z;
-                var left_elbow_x = frame.people[0].joints[6].position.x;
-                var left_elbow_y = frame.people[0].joints[6].position.y;
-                var left_elbow_z = frame.people[0].joints[6].position.z;
-                var left_wrist_x = frame.people[0].joints[7].position.x;
-                var left_wrist_y = frame.people[0].joints[7].position.y;
-                var left_wrist_z = frame.people[0].joints[7].position.z;
+                var chest_x = positions.joints[2].position.x;
+                var chest_y = positions.joints[2].position.y;
+                var chest_z = positions.joints[2].position.z;
+                var nose_x = positions.joints[27].position.x;
+                var nose_y = positions.joints[27].position.y;
+                var nose_z = positions.joints[27].position.z;
+                var left_elbow_x = positions.joints[6].position.x;
+                var left_elbow_y = positions.joints[6].position.y;
+                var left_elbow_z = positions.joints[6].position.z;
+                var left_wrist_x = positions.joints[7].position.x;
+                var left_wrist_y = positions.joints[7].position.y;
+                var left_wrist_z = positions.joints[7].position.z;
 
-                var left_knee_x = frame.people[0].joints[19].position.x;
-                var left_knee_y = frame.people[0].joints[19].position.y;
-                var left_knee_z = frame.people[0].joints[19].position.z;
+                var left_knee_x = positions.joints[19].position.x;
+                var left_knee_y = positions.joints[19].position.y;
+                var left_knee_z = positions.joints[19].position.z;
 
-                var pelvis_x = frame.people[0].joints[0].position.x;
-                var pelvis_y = frame.people[0].joints[0].position.y;
-                var pelvis_z = frame.people[0].joints[0].position.z;
+                var pelvis_x = positions.joints[0].position.x;
+                var pelvis_y = positions.joints[0].position.y;
+                var pelvis_z = positions.joints[0].position.z;
 
-                var right_knee_x = frame.people[0].joints[23].position.x;
-                var right_knee_y = frame.people[0].joints[23].position.y;
-                var right_knee_z = frame.people[0].joints[23].position.z;
+                var right_knee_x = positions.joints[23].position.x;
+                var right_knee_y = positions.joints[23].position.y;
+                var right_knee_z = positions.joints[23].position.z;
 
-                var right_elbow_x = frame.people[0].joints[13].position.x;
-                var right_elbow_y = frame.people[0].joints[13].position.y;
-                var right_elbow_z = frame.people[0].joints[13].position.z;
-                var right_wrist_x = frame.people[0].joints[14].position.x;
-                var right_wrist_y = frame.people[0].joints[14].position.y;
-                var right_wrist_z = frame.people[0].joints[14].position.z;
+                var right_elbow_x = positions.joints[13].position.x;
+                var right_elbow_y = positions.joints[13].position.y;
+                var right_elbow_z = positions.joints[13].position.z;
+                var right_wrist_x = positions.joints[14].position.x;
+                var right_wrist_y = positions.joints[14].position.y;
+                var right_wrist_z = positions.joints[14].position.z;
 
                 if (left_elbow_y < chest_y && left_wrist_y < nose_y && left_wrist_x >left_elbow_x) {
                 // console.log(left_elbow_y + ' ' + chest_y + ' ' + left_wrist_y + ' ' + nose_y + ' ' + left_wrist_x + ' ' + left_elbow_x);
@@ -95,11 +97,8 @@ var frames = {
                         succeeded = false;
                         count = 10;
                     } else {
-                        
-                    }
-                    setTimeout(function () {
-                        window.location.href = 'page3.html'; // redirect to different_page.html after 3 seconds
-                    }, 3000);
+
+                    };
                 } else if (count < 0) {
                     console.log('Counter should not go below 0');
                     clearInterval(timer); // stop the interval
@@ -124,6 +123,12 @@ var frames = {
         } else {
             console.log('TIMER IS ALREADY RUNNING!');
         }
+	},
+    get_left_wrist_command: function (frame) {
+		if (frame.people.length < 1) {
+			return null;
+		}
+		positions = frame.people[0];
 	},
 };
 
